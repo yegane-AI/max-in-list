b462d30d23e1ce043bd6f8a5260238dec64777f5
yegane-AI <51883127+yegane-AI@users.noreply.github.com>
2019-08-18 02:08:41 +0430

    Add files via upload
    Those exe files! I like them :) and I hope to provide a graphically environment for my projects! I'll try my best.

---
+++ maxinlist.py
@@ -0,0 +1,9 @@
+#find max
+x = int(input("How many numbers would you type: "))
+list=[]
+for i in range(1,x+1):
+    y = int(input("Type a number: "))
+    list.append(y)
+print("maximum is: ",max(list))
+
+input("Press Enter to exit ")
